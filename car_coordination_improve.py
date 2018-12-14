5f3eaaf5051dbcd5400e1973b30199644b8ca697
Zichao <zichao2018@gmail.com>
2018-12-13 20:54:23 -0500

    Update doc

--- car_coordination_improve.py
+++ car_coordination_improve.py
@@ -3,13 +3,8 @@ import networkx as nx
 from cross import Car
 import pygame
 
+
 def get_the_location(crosses, cross_roads: List[CrossRoad], car: Car, car_length: int = 10):
-    # problem: at the end, there is no node, so action[0] will be an error but it may not reach the destination
-    # initial_distance is the original distance to the first node. It is constant
-    #  column shi xiang xia
-    # the initial edge distance
-    # edge_length_x = 50  # the initial edge length/ the non-connected part
-    # edge_length_y = 50  # the initial edge length/ the non-connected part
     try:
         location = [-1, -1]
         dis_betw_car = 2
@@ -18,12 +13,13 @@ def get_the_location(crosses, cross_roads: List[CrossRoad], car: Car, car_length
             if node is car.actions[0]:
                 location[0] = count
                 break
-        print(location[0])
         # find the queue location
         for count, direction in enumerate(car.actions[0].all):
             if car.dest is direction:
                 location[1] = count
                 break
+        print("Debug:", location[0], location[1], car.actions[0],car.arrived)
+
         # uses the cross to find the origin, the middle
         origin = pygame.Rect(crosses[location[0]].x + crosses[location[0]].width // 2 - car_length // 2,
                              crosses[location[0]].y + crosses[location[0]].height // 2 - - car_length // 2, car_length,
@@ -36,7 +32,7 @@ def get_the_location(crosses, cross_roads: List[CrossRoad], car: Car, car_length
         if location[1] != -1:
             if location[1] == 0:
                 # it is north
-                origin.y -= 0.5 * car_length - car.dist_to_cross * (car_length + dis_betw_car) + - crosses[
+                origin.y -= 0.5 * car_length + car.dist_to_cross * (car_length + dis_betw_car) - crosses[
                     location[0]].height // 2
                 return origin
             elif location[1] == 1:
@@ -46,7 +42,7 @@ def get_the_location(crosses, cross_roads: List[CrossRoad], car: Car, car_length
                 return origin
             elif location[1] == 2:
                 # it is west
-                origin.x -= 0.5 * car_length - car.dist_to_cross * (car_length + dis_betw_car) - crosses[
+                origin.x -= 0.5 * car_length + car.dist_to_cross * (car_length + dis_betw_car) - crosses[
                     location[0]].width // 2
                 return origin
             elif location[1] == 3:
